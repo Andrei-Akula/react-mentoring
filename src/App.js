46da73d4070e1b72a440287f8f4cd75ab23f038e
aakula <aakula@edmunds.com>
2016-11-24 13:56:14 +0300

    play with some components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+class WelcomeCls extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
 class App extends Component {
   render() {
     let a = [1,2,3];
@@ -14,6 +24,8 @@ class App extends Component {
         <p className="App-intro">
             es6 works: {a.join('')}
         </p>
+        <Welcome name="Sara" />
+        <WelcomeCls name="Abram" />
       </div>
     );
   }
