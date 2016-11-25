fa608b25cd18adceadf3c070b089df557f3f6864
aakula <aakula@edmunds.com>
2016-11-25 09:17:41 +0300

    Add header

--- src/parts/app/App.js
+++ src/parts/app/App.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
+import Header from 'src/parts/header/Header';
 import Todo from 'src/parts/todo/Todo';
-import logo from './logo.svg';
 import './App.css';
-
-function AppHeader(props) {
-    return (
-        <div className="AppHeader">
-          <img src={props.logoSrc} className="App-logo" alt="logo" />
-          <h2>React Mentoring Program</h2>
-        </div>
-    );
-}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <AppHeader logoSrc={logo} />
+        <Header />
         <Todo />
       </div>
     );
--- src/parts/todo/Todo.js
+++ src/parts/todo/Todo.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 class Todo extends Component {
   render() {
     return (
-      <div className="Todo">
+      <section className="Todo">
         <p>Todo Component</p>
-      </div>
+        <a className="button is-primary">Primary</a>
+      </section>
     );
   }
 }
